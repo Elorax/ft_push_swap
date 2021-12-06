43616ef84f2797def03373efef7d2a480759515a
Antoine BIERSOHN <abiersoh@e2r1p13.clusters.42paris.fr>
2021-12-06 23:54:56 +0100

    checker normed

--- bonus/checker.c
+++ bonus/checker.c
@@ -1,110 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abiersoh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 23:47:13 by abiersoh          #+#    #+#             */
+/*   Updated: 2021/12/06 23:47:52 by abiersoh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../ft_push_swap.h"
 #include "get_next_line.h"
+
+void	check_str(t_itab *a, t_itab *b, char *str)
+{
+	if (!ft_strncmp(str, "sa\n", 3))
+		swap(*a, 0);
+	else if (!ft_strncmp(str, "sb\n", 3))
+		swap(*b, 0);
+	else if (!ft_strncmp(str, "ss\n", 3))
+		(swap(*a, 0), swap(*b, 0));
+	else if (!ft_strncmp(str, "pa\n", 3))
+		push(a, b, 0);
+	else if (!ft_strncmp(str, "pb\n", 3))
+		push(b, a, 0);
+	else if (!ft_strncmp(str, "ra\n", 3))
+		rotate(*a, 0);
+	else if (!ft_strncmp(str, "rb\n", 3))
+		rotate(*b, 0);
+	else if (!ft_strncmp(str, "rr\n", 3))
+		(rotate(*a, 0), rotate(*b, 0));
+	else if (!ft_strncmp(str, "rrr\n", 4))
+		(reverse_rotate(*b, 0), reverse_rotate(*a, 0));
+	else if (!ft_strncmp(str, "rra\n", 4))
+		reverse_rotate(*a, 0);
+	else if (!ft_strncmp(str, "rrb\n", 4))
+		reverse_rotate(*b, 0);
+	else
+		exit(free(str), write(1, "Error\n", 6), EXIT_SUCCESS);
+	str = (free(str), get_next_line(0));
+}
 
 void	checker(t_itab *a, t_itab *b)
 {
 	char	*str;
-	while ((str = get_next_line(0)) != NULL)
-	{
-		if (!ft_strncmp(str, "sa\n", 3))
-			swap(*a, 0);
-		else if (!ft_strncmp(str, "sb\n", 3))
-			swap(*b, 0);
-		else if (!ft_strncmp(str, "ss\n", 3))
-			(swap(*a, 0), swap(*b, 0));
-		else if (!ft_strncmp(str, "pa\n", 3))
-			push(a, b, 0);
-		else if (!ft_strncmp(str, "pb\n", 3))
-			push(b, a, 0);
-		else if (!ft_strncmp(str, "ra\n", 3))
-			rotate(*a, 0);
-		else if (!ft_strncmp(str, "rb\n", 3))
-			rotate(*b, 0);
-		else if (!ft_strncmp(str, "rr\n", 3))
-			(rotate(*a, 0), rotate(*b, 0));
-		else if (!ft_strncmp(str, "rrr\n", 4))
-			(reverse_rotate(*b, 0), reverse_rotate(*a, 0));
-		else if (!ft_strncmp(str, "rra\n", 4))
-			reverse_rotate(*a, 0);
-		else if (!ft_strncmp(str, "rrb\n", 4))
-			reverse_rotate(*b, 0);
-		/*else
-		{
-			free(str);
-			write(2, "Wrong output", 7);
-			return ;
-		}*/
-		free(str);
-	}
+
+	str = get_next_line(0);
+	if (ft_is_stack_sorted(a) && (str != NULL))
+		write(1, "KO\n", 3);
+	while (str != NULL)
+		check_str(a, b, str);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_itab a;
-	t_itab b;
-
+	t_itab	a;
+	t_itab	b;
 	char	*str;
-	char	*str2;
-	if (ac < 2)
-	{
-		str = get_next_line(1);
-		str2 = get_next_line(2);
-		if (str || str2)
-		{
-			printf("KO arg < 2\n");
-			//free(str);
-			//free(str2);
-			return (0);
-		}
-		else
-		{
-			printf("OK arg < 2\n");
-			return (0);
-		}
-	}
-	else if (!ft_init_stacks(ac - 1, &av[1], &a, &b) || !ft_is_stack_valid(a))
-	{
-		str = get_next_line(2);
-		if(!ft_strncmp(str, "Error\n", 6))
-		{
-			printf("OK error bien ecrite\n");
-			//free(str);
-			return 0;
-		}
-		else
-		{
-			printf("KO pas derreur ecrite\n");
-			//free(str);
-			return 0;
-		}
-	}
-	else if (ft_is_stack_sorted(a))
-	{
-		str = get_next_line(1);
-		if(!str)
-		{
-			printf("OK champ vide\n");
-		}
-		else
-		{
-			printf("KO champ non vide\n");
-			//free(str);
-			return 0;
-		}
-	}
+
+	if (!ft_init_stacks(ac - 1, &av[1], &a, &b) || !ft_is_stack_valid(a))
+		return (write(1, "Error\n", 6), free(a.tab), free(b.tab), 0);
 	checker(&a, &b);
 	if (ft_is_stack_sorted(a))
-	{
-		printf("OK bien trie\n");
-		//free(a.tab);
-		//free(b.tab);
-		return 0;
-	}
+		write(1, "OK\n", 3);
 	else
-	{
-		printf("KO mal triee\n");
-		//free(a.tab);
-		//free(b.tab);
-		return 0;
-	}
+		write(1, "KO\n", 3);
+	free(a.tab);
+	free(b.tab);
 }
